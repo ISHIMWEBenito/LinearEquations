8ae1a6016c839e6fff0bba7da43c82bf0b028c49
Benish-31 <benishimwe31@gmail.com>
2021-11-11 23:24:43 +0100

    Add project files.

---
+++ LinearEquations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LinearEquations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type linear equations in augmented matrix notation: a1 a2... aN d, \n where a1..N are coefficients and d is constant");
+            Console.WriteLine("Type END to finish entering equations");
+            Console.Write("Eq #1:");
+            Console.Write("Eq #2:");
+            Console.Write("Eq #3: END");
+            Console.WriteLine("You have entered the following equations:");
+            Console.Write("Eq #1:" +"*x1" + "x2" + "=" );
+            Console.Write("Eq #2:" +"*x1" + "x2" + "=" );
+            
+
+        }
+    }
+}
